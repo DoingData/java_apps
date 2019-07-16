be14b4408a8532af59212b40e396b160fe383d93
Usman <usman.asif.jrvs.ca@gmail.com>
2019-07-16 20:15:38 +0000

    updates error cleared

--- src/main/java/ca/jrvs/apps/twitter/TwitterCLI.java
+++ src/main/java/ca/jrvs/apps/twitter/TwitterCLI.java
@@ -2,8 +2,8 @@ package ca.jrvs.apps.twitter;
 
 import ca.jrvs.apps.twitter.Service.TwitterService;
 import ca.jrvs.apps.twitter.Service.TwitterServiceImp;
-import ca.jrvs.apps.twitter.dao.CrdRepo;
-import ca.jrvs.apps.twitter.dao.TwitterResDao;
+import ca.jrvs.apps.twitter.dao.CrdRepository;
+import ca.jrvs.apps.twitter.dao.TwitterRestDao;
 import ca.jrvs.apps.twitter.dao.helper.ApacheHttpHelper;
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 
@@ -12,7 +12,7 @@ public class TwitterCLI {
     public static void main(String[] args) {
 
         HttpHelper httpHelper = new ApacheHttpHelper();
-        CrdRepo dao = new TwitterResDao(httpHelper);
+        CrdRepository dao = new TwitterRestDao(httpHelper);
         TwitterService service = new TwitterServiceImp(dao);
         TwitterCLIRunner runner = new TwitterCLIRunner(service);
         runner.run(args);
