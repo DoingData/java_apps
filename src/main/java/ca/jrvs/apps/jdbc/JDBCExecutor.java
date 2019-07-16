b425dc7a3b83eb2017cef10a2c544550d63c24be
Usman <usman.asif.jrvs.ca@gmail.com>
2019-07-16 16:17:29 +0000

    updated

--- src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,29 +1,61 @@
 package ca.jrvs.apps.jdbc;
 
-import java.net.ConnectException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Connection;
-import java.util.Properties;
+import java.sql.SQLException;
 
 public class JDBCExecutor {
+
     public static void main(String... args) {
-        System.out.println("This is JDBC project");
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
-               "hplussport","postgres","password");
-        try{
+        DataConnectionManager dcm = new DataConnectionManager("localhost",
+                "hplussport", "postgres", "password");
+
+        try {
+
             Connection connection = dcm.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("Select COUNT(*) FROM CUSTOMER");
-            while(resultSet.next()) {
-                System.out.println(resultSet.getInt(1));
+            Customer customer;
+            switch (args[0]) {
+                case "create":
+                    customer = new Customer();
+                    customer.setFirstName(args[1]);
+                    customer.setLastName(args[2]);
+                    customer.setEmail(args[3]);
+                    customer.setPhone(args[4]);
+                    customer.setAddress(args[5]);
+                    customer.setCity(args[6]);
+                    customer.setState(args[7]);
+                    customer.setZipCode(args[8]);
+                    System.out.println(new CustomerDAO(connection).create(customer));
+                    break;
+                case "update":
+                    customer = new Customer();
+                    customer.setId(Long.getLong(args[1]));
+                    customer.setFirstName(args[2]);
+                    customer.setLastName(args[3]);
+                    customer.setEmail(args[4]);
+                    customer.setPhone(args[5]);
+                    customer.setAddress(args[6]);
+                    customer.setCity(args[7]);
+                    customer.setState(args[8]);
+                    customer.setZipCode(args[9]);
+                    System.out.println(new CustomerDAO(connection).update(customer));
+                    break;
+                case "read":
+                    System.out.println(new CustomerDAO(connection).findById(Long.getLong(args[1])));
+                    break;
+                case "order":
+                    System.out.println(new OrderDAO(connection).findById(Integer.valueOf(args[1])));
+                    break;
+                case "delete":
+                    new CustomerDAO(connection).delete(Long.getLong(args[1]));
+                    break;
+                default:
+                    System.out.println("Usage: create|update|read|order|delete");
             }
-            }catch(SQLException e){
-                e.printStackTrace();
-            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
     }
-
-
-
+}
