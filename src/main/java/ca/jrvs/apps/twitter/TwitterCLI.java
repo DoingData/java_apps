93df9c4f0c1c859dd6e2c5b8d27c1d09fd0bd77d
Usman <51926991+UsmanAsifJrvs@users.noreply.github.com>
2019-07-30 02:31:53 -0400

    update

--- src/main/java/ca/jrvs/apps/twitter/TwitterCLI.java
+++ src/main/java/ca/jrvs/apps/twitter/TwitterCLI.java
@@ -1,23 +1,16 @@
 package ca.jrvs.apps.twitter;
-
 import ca.jrvs.apps.twitter.Service.TwitterService;
 import ca.jrvs.apps.twitter.Service.TwitterServiceImp;
 import ca.jrvs.apps.twitter.dao.CrdRepository;
 import ca.jrvs.apps.twitter.dao.TwitterRestDao;
 import ca.jrvs.apps.twitter.dao.helper.ApacheHttpHelper;
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
-
 public class TwitterCLI {
-
     public static void main(String[] args) {
-
         HttpHelper httpHelper = new ApacheHttpHelper();
         CrdRepository dao = new TwitterRestDao(httpHelper);
         TwitterService service = new TwitterServiceImp(dao);
         TwitterCLIRunner runner = new TwitterCLIRunner(service);
         runner.run(args);
-
     }
-
-
 }
